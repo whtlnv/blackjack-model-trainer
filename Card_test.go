fc03442f92610e57ed66091e67862ad5f1892c83
whtlnv <catala.francisco@gmail.com>
2023-07-07 11:44:39 -0600

    UPDATE(Card_test): cover face and number cards

--- Card_test.go
+++ Card_test.go
@@ -6,11 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCardValue(t *testing.T) {
+func TestAceCardValue(t *testing.T) {
 	c := Card{Ace, Spades}
 	
 	got := c.Value()
 	want := []int{1, 11}
+
+	assert.ElementsMatch(t, got, want)
+}
+
+func TestFaceCardValue(t *testing.T) {
+	c := Card{Jack, Hearts}
+	
+	got := c.Value()
+	want := []int{10}
+
+	assert.ElementsMatch(t, got, want)
+}
+
+func TestNumberCardValue(t *testing.T) {
+	c := Card{Two, Clubs}
+	
+	got := c.Value()
+	want := []int{2}
 
 	assert.ElementsMatch(t, got, want)
 }
