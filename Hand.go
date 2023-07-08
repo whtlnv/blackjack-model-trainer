29c981366e32b9fbb87456724d785f140488d6b0
whtlnv <catala.francisco@gmail.com>
2023-07-08 02:28:57 -0600

    UPDATE(Hand): score

--- Hand.go
+++ Hand.go
@@ -5,23 +5,37 @@ import "github.com/samber/lo"
 type Hand []Card
 
 func (hand Hand) Values() []int {
-	var sums []int
-	var calculateSum func(cards []Card, index int, currentSum int)
+	var calculateSum func(cards []Card, index int, currentSum int) []int
 
-	calculateSum = func(cards []Card, index int, currentSum int) {
+	calculateSum = func(cards []Card, index int, currentSum int) []int {
 		if index >= len(cards) {
-			sums = append(sums, currentSum)
-			return
+			return []int{currentSum}
 		}
 
 		card := cards[index]
 		values := card.Value()
+		var sums []int
 		for _, value := range values {
-			calculateSum(cards, index+1, currentSum+value)
+			moreSums := calculateSum(cards, index+1, currentSum+value)
+			sums = append(sums, moreSums...)
 		}
+
+		return sums
 	}
 
-	calculateSum(hand, 0, 0)
+	return lo.Uniq(calculateSum(hand, 0, 0))
+}
 
-	return lo.Uniq(sums)
+func (hand Hand) Score() (score int, isBusted bool) {
+	values := hand.Values()
+
+	notBusted := lo.Filter(values, func(value int, _ int) bool {
+		return value <= 21
+	})
+
+	if len(notBusted) == 0 {
+		return lo.Max(values), true
+	}
+
+	return lo.Max(notBusted), false
 }
--- Hand_test.go
+++ Hand_test.go
@@ -46,3 +46,61 @@ func TestHandValues(t *testing.T) {
 		assert.Equal(t, want, got)
 	})
 }
+
+func TestHandScore(t *testing.T) {
+	t.Run("Should return the highest score of a hand", func(t *testing.T) {
+		hand := Hand{
+			Card{Two, Spades},
+			Card{Seven, Hearts},
+			Card{Queen, Diamonds},
+		}
+
+		got, isBusted := hand.Score()
+		want := 19
+
+		assert.Equal(t, want, got)
+		assert.Equal(t, false, isBusted)
+	})
+
+	t.Run("Should return the correct score for Ace", func(t *testing.T) {
+		hand := Hand{
+			Card{Ace, Spades},
+			Card{King, Hearts},
+			Card{Queen, Diamonds},
+		}
+
+		got, isBusted := hand.Score()
+		want := 21
+
+		assert.Equal(t, want, got)
+		assert.Equal(t, false, isBusted)
+	})
+
+	t.Run("Should return the correct score for several Aces", func(t *testing.T) {
+		hand := Hand{
+			Card{Ace, Spades},
+			Card{Ace, Spades},
+			Card{Three, Diamonds},
+		}
+
+		got, isBusted := hand.Score()
+		want := 15
+
+		assert.Equal(t, want, got)
+		assert.Equal(t, false, isBusted)
+	})
+
+	t.Run("Should return true if busted", func(t *testing.T) {
+		hand := Hand{
+			Card{Queen, Spades},
+			Card{Queen, Spades},
+			Card{Queen, Diamonds},
+		}
+
+		got, isBusted := hand.Score()
+		want := 30
+
+		assert.Equal(t, want, got)
+		assert.Equal(t, true, isBusted)
+	})
+}
