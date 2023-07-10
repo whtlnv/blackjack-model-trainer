bf362f947bfe14e64e53371ed275a646ae78d6c8
whtlnv <catala.francisco@gmail.com>
2023-07-10 13:46:52 -0600

    FIX(strategy): read rows not columns

--- Strategy.go
+++ Strategy.go
@@ -72,9 +72,9 @@ const (
 )
 
 type Strategy struct {
-	PairMap map[DealerHand]map[PlayerHand]PlayerAction
-	SoftMap map[DealerHand]map[PlayerHand]PlayerAction
-	HardMap map[DealerHand]map[PlayerHand]PlayerAction
+	PairMap map[PlayerHand]map[DealerHand]PlayerAction
+	SoftMap map[PlayerHand]map[DealerHand]PlayerAction
+	HardMap map[PlayerHand]map[DealerHand]PlayerAction
 }
 
 // Factory
@@ -94,14 +94,14 @@ func (strategy *Strategy) Play(playerHand Hand, dealerHand Hand) PlayerAction {
 	playerScore, _ := playerHand.Score()
 
 	if playerHand.IsPair() {
-		return strategy.PairMap[DealerHand(dealerScore.Hard)][PlayerHand(playerScore.Hard)]
+		return strategy.PairMap[PlayerHand(playerScore.Hard)][DealerHand(dealerScore.Hard)]
 	}
 
 	if playerHand.HasSoftValue() {
-		return strategy.SoftMap[DealerHand(dealerScore.Hard)][PlayerHand(playerScore.Soft)]
+		return strategy.SoftMap[PlayerHand(playerScore.Hard)][DealerHand(dealerScore.Soft)]
 	}
 
-	return strategy.HardMap[DealerHand(dealerScore.Hard)][PlayerHand(playerScore.Hard)]
+	return strategy.HardMap[PlayerHand(playerScore.Hard)][DealerHand(dealerScore.Hard)]
 }
 
 // Private methods
@@ -109,7 +109,7 @@ func (strategy *Strategy) Play(playerHand Hand, dealerHand Hand) PlayerAction {
 func (strategy *Strategy) parseActionMap(raw []byte) {
 	dealerHandCount := 10
 	playerHardHandCount := 16
-	playerSoftHandCount := 9
+	playerSoftHandCount := 8
 	// playerPairHandCount := 10
 
 	rawHardMapStartsAt := 0
@@ -125,21 +125,21 @@ func (strategy *Strategy) parseActionMap(raw []byte) {
 	strategy.PairMap = strategy.parsePairMap(rawPairMap)
 }
 
-func (strategy *Strategy) parseHardMap(slicedRaw []byte) map[DealerHand]map[PlayerHand]PlayerAction {
+func (strategy *Strategy) parseHardMap(slicedRaw []byte) map[PlayerHand]map[DealerHand]PlayerAction {
 	columns := [10]DealerHand{DealerTwo, DealerThree, DealerFour, DealerFive, DealerSix, DealerSeven, DealerEight, DealerNine, DealerTen, DealerAce}
 	rows := [16]PlayerHand{HardFive, HardSix, HardSeven, HardEight, HardNine, HardTen, HardEleven, HardTwelve, HardThirteen, HardFourteen, HardFifteen, HardSixteen, HardSeventeen, HardEighteen, HardNineteen, HardTwenty}
 
 	return stringToMap(slicedRaw, columns[:], rows[:])
 }
 
-func (strategy *Strategy) parseSoftMap(slicedRaw []byte) map[DealerHand]map[PlayerHand]PlayerAction {
+func (strategy *Strategy) parseSoftMap(slicedRaw []byte) map[PlayerHand]map[DealerHand]PlayerAction {
 	columns := [10]DealerHand{DealerTwo, DealerThree, DealerFour, DealerFive, DealerSix, DealerSeven, DealerEight, DealerNine, DealerTen, DealerAce}
-	rows := [9]PlayerHand{SoftThirteen, SoftFourteen, SoftFifteen, SoftSixteen, SoftSeventeen, SoftEighteen, SoftNineteen, SoftTwenty}
+	rows := [8]PlayerHand{SoftThirteen, SoftFourteen, SoftFifteen, SoftSixteen, SoftSeventeen, SoftEighteen, SoftNineteen, SoftTwenty}
 
 	return stringToMap(slicedRaw, columns[:], rows[:])
 }
 
-func (strategy *Strategy) parsePairMap(slicedRaw []byte) map[DealerHand]map[PlayerHand]PlayerAction {
+func (strategy *Strategy) parsePairMap(slicedRaw []byte) map[PlayerHand]map[DealerHand]PlayerAction {
 	columns := [10]DealerHand{DealerTwo, DealerThree, DealerFour, DealerFive, DealerSix, DealerSeven, DealerEight, DealerNine, DealerTen, DealerAce}
 	rows := [10]PlayerHand{PairTwos, PairThrees, PairFours, PairFives, PairSixes, PairSevens, PairEights, PairNines, PairTens, PairAces}
 
@@ -148,15 +148,15 @@ func (strategy *Strategy) parsePairMap(slicedRaw []byte) map[DealerHand]map[Play
 
 // Helper methods
 
-func stringToMap(raw []byte, columns []DealerHand, rows []PlayerHand) map[DealerHand]map[PlayerHand]PlayerAction {
-	parsedMap := make(map[DealerHand]map[PlayerHand]PlayerAction)
+func stringToMap(raw []byte, columns []DealerHand, rows []PlayerHand) map[PlayerHand]map[DealerHand]PlayerAction {
+	parsedMap := make(map[PlayerHand]map[DealerHand]PlayerAction)
 
 	cursor := 0
-	for _, column := range columns {
-		parsedMap[column] = make(map[PlayerHand]PlayerAction)
+	for _, row := range rows {
+		parsedMap[row] = make(map[DealerHand]PlayerAction)
 
-		for _, row := range rows {
-			parsedMap[column][row] = PlayerAction(raw[cursor])
+		for _, column := range columns {
+			parsedMap[row][column] = PlayerAction(raw[cursor])
 			cursor += 1
 		}
 	}
--- Strategy_test.go
+++ Strategy_test.go
@@ -2,23 +2,110 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func openTextFile(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	fileSize := fileInfo.Size()
+	fileContent := make([]byte, fileSize)
+
+	_, err = file.Read(fileContent)
+	if err != nil {
+		return nil, err
+	}
+
+	return fileContent, nil
+}
+
+func getTestStrategy() ([]byte, error) {
+	cwd, cwdErr := os.Getwd()
+	if cwdErr != nil {
+		return nil, cwdErr
+	}
+
+	testStrategyFileName := cwd + "/__mock_data__/strategies/ideal.strategy"
+	raw, readError := openTextFile(testStrategyFileName)
+
+	if readError != nil {
+		return nil, readError
+	}
+
+	// Remove line breaks and spaces
+	noLineBreaks := bytes.ReplaceAll(raw, []byte("\n"), []byte(""))
+	noSpaces := bytes.ReplaceAll(noLineBreaks, []byte(" "), []byte(""))
+
+	if len(noSpaces) != 340 {
+		return nil, fmt.Errorf("Expected strategy length to be 340, got %d", len(noSpaces))
+	}
+
+	return noSpaces, nil
+}
+
 func TestStrategyParsing(t *testing.T) {
-	t.Run("Should parse a strategy from string", func(t *testing.T) {
-		// Hit every time
-		raw := bytes.Repeat([]byte("H"), 350)
-		strategy := NewStrategy(raw)
+	raw, err := getTestStrategy()
+	if err != nil {
+		t.Fatal(err)
+	}
+	strategy := NewStrategy(raw)
 
-		playerHand := Hand{Card{Ace, Spades}, Card{Ace, Hearts}}
-		dealerHand := Hand{Card{Two, Clubs}}
+	testCases := []struct {
+		desc           string
+		playerHand     Hand
+		dealerHand     Hand
+		expectedAction PlayerAction
+	}{
+		{
+			desc:           "Should handle hard values",
+			playerHand:     Hand{Card{King, Clubs}, Card{Five, Clubs}},
+			dealerHand:     Hand{Card{Two, Clubs}},
+			expectedAction: Stay,
+		},
+		{
+			desc:           "Should handle soft values",
+			playerHand:     Hand{Card{Ace, Clubs}, Card{Five, Clubs}},
+			dealerHand:     Hand{Card{Six, Clubs}},
+			expectedAction: Double,
+		},
+		{
+			desc:           "Should handle pairs",
+			playerHand:     Hand{Card{Four, Clubs}, Card{Four, Hearts}},
+			dealerHand:     Hand{Card{Four, Clubs}},
+			expectedAction: Hit,
+		},
+		{
+			desc:           "Should handle pairs",
+			playerHand:     Hand{Card{Nine, Clubs}, Card{Nine, Hearts}},
+			dealerHand:     Hand{Card{Seven, Clubs}},
+			expectedAction: Stay,
+		},
+		{
+			desc:           "Should handle two aces",
+			playerHand:     Hand{Card{Ace, Spades}, Card{Ace, Hearts}},
+			dealerHand:     Hand{Card{Two, Clubs}},
+			expectedAction: Split,
+		},
+	}
 
-		got := strategy.Play(playerHand, dealerHand)
-		want := Hit
-
-		assert.Equal(t, want, got)
-	})
+	for _, testCase := range testCases {
+		t.Run(testCase.desc, func(t *testing.T) {
+			got := strategy.Play(testCase.playerHand, testCase.dealerHand)
+			want := testCase.expectedAction
+			assert.Equal(t, want, got)
+		})
+	}
 }
