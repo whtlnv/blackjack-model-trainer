a14a6a5d40292cfd95e2e35d58b5ab2d6e1c04d1
whtlnv <catala.francisco@gmail.com>
2023-07-12 17:04:35 -0600

    ADD(cmd/train/main): dummy main()

---
+++ cmd/train/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/whtlnv/blackjack-model-trainer/pkg/blackjack"
+)
+
+func main() {
+	shoe := blackjack.NewShoe(1)
+
+	fmt.Println("Got a shoe")
+	fmt.Println(shoe)
+
+	rawStrategy := bytes.Repeat([]byte("H"), blackjack.HandCount)
+	strategy, _ := blackjack.NewStrategy(rawStrategy)
+	player := blackjack.NewPlayer(strategy)
+
+	willBet, ammount := player.ShouldBet()
+
+	fmt.Println("Got a player")
+	fmt.Println(willBet, ammount)
+}
