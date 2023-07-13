c03919bf4439e4ba2a77cf4f087ae06069e8a68c
whtlnv <catala.francisco@gmail.com>
2023-07-12 20:08:46 -0600

    UPDATE(Player): set bankroll

--- pkg/blackjack/Player.go
+++ pkg/blackjack/Player.go
@@ -2,12 +2,17 @@ package blackjack
 
 type Player struct {
 	strategy Strategy
+	Bankroll int
 }
 
 // Factory
 
 func NewPlayer(strategy Strategy) Player {
-	return Player{strategy}
+	player := Player{}
+	player.strategy = strategy
+	player.Bankroll = strategy.InitialBankroll
+
+	return player
 }
 
 // Public methods
--- pkg/blackjack/Player_test.go
+++ pkg/blackjack/Player_test.go
@@ -7,6 +7,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestPlayerInitialization(t *testing.T) {
+	t.Run("Should set player initial bankroll", func(t *testing.T) {
+		rawStrategy := bytes.Repeat([]byte("H"), HandCount)
+		strategy, _ := NewStrategy(rawStrategy)
+		player := NewPlayer(strategy)
+
+		assert.Equal(t, strategy.InitialBankroll, player.Bankroll)
+	})
+}
+
 func TestPlayerTurn(t *testing.T) {
 	t.Run("Should decide to play a hand", func(t *testing.T) {
 		rawStrategy := bytes.Repeat([]byte("H"), HandCount)
